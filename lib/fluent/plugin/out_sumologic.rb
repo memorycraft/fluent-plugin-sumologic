75b30761c0466da9409a553da7c9e743790b1928
memorycraft <memorycraft@gmail.com>
2013-04-30 14:55:54 +0900

    add test helper

--- lib/fluent/plugin/out_sumologic.rb
+++ lib/fluent/plugin/out_sumologic.rb
@@ -1,9 +1,45 @@
-require "fluent/plugin/sumologic/version"
+# encoding: UTF-8
+require 'net/http'
+require 'date'
 
-module Fluent
-  module Plugin
-    module Sumologic
-      # Your code goes here...
+class Fluent::SumologicOutput< Fluent::BufferedOutput
+  Fluent::Plugin.register_output('sumologic', self)
+
+  config_param :host, :string,  :default => 'localhost'
+  config_param :port, :integer, :default => 9200
+  config_param :path, :string,  :default => '/'
+
+  def initialize
+    super
+  end
+
+  def configure(conf)
+    super
+  end
+
+  def start
+    super
+  end
+
+  def format(tag, time, record)
+    [tag, time, record].to_msgpack
+  end
+
+  def shutdown
+    super
+  end
+
+  def write(chunk)
+    bulk_message = []
+
+    chunk.msgpack_each do |tag, time, record|
+      bulk_message << record.to_json
     end
+    bulk_message << ""
+
+    http = Net::HTTP.new(@host, @port.to_i)
+    request = Net::HTTP::Post.new(@path)
+    request.body = bulk_message.join("\n")
+    http.request(request)
   end
 end
